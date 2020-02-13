841fe56da09bc904f06ba92e47cdb182be043a7d
Alek-FFF <olfil777@gmail.com>
2020-02-13 14:16:27 +0300

    added product section

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ let gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin');
+    
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
@@ -21,7 +22,8 @@ gulp.task('sass', function(){
 gulp.task('script', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/mixitup/dist/mixitup.js'
   ])
   .pipe(concat('libs.min.js'))
   .pipe(uglify())
