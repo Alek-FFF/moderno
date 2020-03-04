da705a948a49df3a1d45e61edc30855b5323cb42
Alek-FFF <olfil777@gmail.com>
2020-03-04 12:21:25 +0300

    added product-one page

--- app/js/main.js
+++ app/js/main.js
@@ -45,6 +45,15 @@ $(function(){
     $('.header__box').toggleClass('active');
   });
 
+  $('.product-one__tab .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tab').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tab .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
 
 
   var mixer = mixitup('.products__inner-box');
